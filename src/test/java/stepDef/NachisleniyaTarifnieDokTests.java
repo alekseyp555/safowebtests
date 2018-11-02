f41f97aeb27e729696f0e2094030e7dd03da5658
alekseyp <park.aleksey@gmail.com>
2018-11-02 14:01:05 +0300

    Доработки по тестам "начисления и тарифные документы"
    Доработал локаторы, переделан выбор даты в календаре.
    Нужно доделать выбор в списке компании

--- src/test/java/stepDef/NachisleniyaTarifnieDokTests.java
+++ src/test/java/stepDef/NachisleniyaTarifnieDokTests.java
@@ -4,9 +4,11 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class NachisleniyaTarifnieDokTests extends TestBase{
@@ -31,17 +33,37 @@ public class NachisleniyaTarifnieDokTests extends TestBase{
 
     @When("Выбрали дату {localdate}")
     public void выбрали_дату_с(LocalDate localdate) throws Throwable{
+
         //driver.findElement(By.name("ch_dt1")).sendKeys("localdate"); //css locators
-        driver.findElement(By.cssSelector("div:nth-of-type(3) > input")).click();
+        driver.findElement(By.cssSelector("div:nth-of-type(3) > input")).click(); //css locators
         driver.findElement(By.cssSelector("div:nth-of-type(3) > input")).clear();
-        driver.findElement(By.cssSelector("div:nth-of-type(3) > input")).sendKeys(localdate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))); //дата 01.09.2018 смотреть фичу
+        //driver.findElement(By.cssSelector("div:nth-of-type(3) > input")).sendKeys(localdate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))); //дата 01.09.2018 смотреть фичу
+        driver.findElement(By.xpath("//div[3]/img")).click(); //клик календарь
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); //ждем
+        driver.findElement(By.cssSelector("td.x-date-left")).click(); //css locator клик 1 месяц назад
+        driver.findElement(By.cssSelector("td.x-date-left")).click(); //css locator клик 2 месяц назад
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); //ждем
+
+        //WebElement kalendar = driver.findElement(By.cssSelector("#ext-comp-1860")); //локатор для таблицы
+        //WebElement kalendar = driver.findElement(By.id("ext-comp-1862")); //не работает
+        WebElement kalendar = driver.findElement(By.xpath("//*[@id='ext-comp-1862']/ul/li/div/table/tbody")); //валидный xpath для календаря
+        List <WebElement> rows = kalendar.findElements(By.tagName("tr"));
+        List <WebElement> columns = kalendar.findElements(By.tagName("td"));
+        for (WebElement cell: columns){
+            //Select 10th Date
+            if (cell.getText().equals("10")){
+                cell.findElement(By.linkText("10")).click();
+                break;
+            }
+        }
+        //driver.findElement(By.cssSelector("div:nth-of-type(3) > input")).sendKeys("localdate"); //дата 01.09.2018 смотреть фичу
         //driver.findElement(By.cssSelector("div:nth-of-type(3) > input")).sendKeys("localdate");
         //driver.findElement(By.xpath("//div[3]/input")).click(); //xpath locators
         //driver.findElement(By.xpath("//div[3]/input")).clear();
         //driver.findElement(By.xpath("//div[3]/input")).sendKeys(localdate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))); //дата 01.09.2018
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         Thread.sleep(5000);
-        System.out.println(localdate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        System.out.println(localdate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))); //форматируем дату
     }
 
     @When("Клик Применить начисления")
@@ -62,7 +84,7 @@ public class NachisleniyaTarifnieDokTests extends TestBase{
     @Then("Выбрать счет")
     public void выбрать_счет() throws Throwable {
         //driver.findElement(By.cssSelector("div[class*=x-grid3-row]:nth-child(2)")).click(); //выбираем второй элемент в списке документов счетов
-        //driver.findElement(By.cssSelector("div[class*=x-grid3-row]:nth-of-type(5)")).click(); //выбираем второй элемент в списке документов счетов
+        driver.findElement(By.cssSelector("div[class*=x-grid3-row]:nth-of-type(5)")).click(); //выбираем второй элемент в списке документов счетов
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); //ожидание загрузки страницы
         Thread.sleep(5000);
         waitForPageLoadComplete(driver);
@@ -79,14 +101,20 @@ public class NachisleniyaTarifnieDokTests extends TestBase{
         //driver.findElement(By.xpath("//li[2]/a[2]/em/span/span")).click(); // xpath клик на Излишне начисленные комиссии
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); //ожидание загрузки страницы
         Thread.sleep(5000);
-        waitForPageLoadComplete(driver); //Ждем загрузку комиссии
+        //waitForPageLoadComplete(driver); //Ждем загрузку комиссии
     }
 
     @Then("Выбрать компанию")
     public void выбрать_компанию() throws Throwable {
-        driver.findElement(By.cssSelector("div[class*=x-grid3-row]:nth-of-type(5)")).click(); //выбираем 5й элемент в списке документов счетов
+        //driver.findElement(By.cssSelector("div[class*=x-grid3-row]:nth-of-type(5)")).click(); //выбираем 5й элемент в списке документов счетов
+        //driver.findElement(By.cssSelector("div[class*=x-grid3-row]:nth-child(2)")).click(); //выбираем 2й элемент в списке документов счетов
+       // WebElement baseTable = driver.findElement(By.xpath("//div[5]/table/tbody/tr/td/div"));
+       // List<WebElement> rows = baseTable.findElements(By.cssSelector("div[class*=x-grid3-row]"));
+        //driver.findElement(By.xpath("//div[5]/table/tbody/tr/td/div")).click(); //xpath выбираем 5 элемент в таблице
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)=concat('ООО ', '\"', 'ЛУДИНГ', '\"', '')])[1]/following::div[3]")).click();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); //ожидание загрузки страницы
         Thread.sleep(5000);
+        waitForPageLoadComplete(driver); //Ждем загрузку компании
     }
 
     @Then("Детализация загрузилась")
--- target/cucumber/report.js
+++ target/cucumber/report.js
@@ -1,4 +1,89 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/java/features/NachisleniyaTarifnieDok.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/java/features/ContrAgents.feature");
+formatter.feature({
+  "name": "Тест для тестирования модуля \"Контрагенты\"",
+  "description": "Залогиненный пользователь должен видеть список контрагентов\nПри клике на контрагента должны отображаться Общие сведения, Дополнительно, Лимиты, Договора, Банковские реквизиты",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Проверка модуля контрагенты",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@accept"
+    },
+    {
+      "name": "@deb"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Выбрать Контрагенты",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ContrAgentsTests.выбрать_Контрагенты()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Клик Применить",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "ContrAgentsTests.клик_Применить()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Список контрагентов появился",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContrAgentsTests.список_контрагентов_появился()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Выбрать контрагента",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContrAgentsTests.выбрать_контрагента()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Загрузились свойства контрагента",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContrAgentsTests.загрузились_свойства_контрагента()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Клик Общие сведения, Дополнительно, Лимиты, Договора, Банковские реквизиты контрагента",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContrAgentsTests.клик_Общие_сведения_Дополнительно_Лимиты_Договора_Банковские_реквизиты_контрагента()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.uri("src/test/java/features/NachisleniyaTarifnieDok.feature");
 formatter.feature({
   "name": "Тест для тестирования модуля \"Начисления и тарифные документы\"",
   "description": "Залогиненный пользователь должен видеть список тарифныз документов, излишне начисленные комиссии, начисления\nПри выборе диапазона дат с 01.07.по 30.10.2018 должны отображатсья тарифные документы за этот период\nПри клике на излишне начисленные комиссии и выборе компании должна отображаться детализация",
