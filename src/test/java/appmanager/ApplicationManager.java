a74a39136a45eb1c315c39d71bb9ccfe9879e7f5
alekseyp <park.aleksey@gmail.com>
2018-12-18 15:26:53 +0300

    доработки по тесту "Платежи"
    доработки по тесту "Платежи"

--- src/test/java/appmanager/ApplicationManager.java
+++ src/test/java/appmanager/ApplicationManager.java
@@ -223,4 +223,17 @@ public class ApplicationManager {
         }
     }
 
+    public boolean isElementPresent(By locatorKey) {
+        try {
+            driver.findElement(locatorKey);
+            return true;
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public boolean isElementVisible(String cssLocator){
+        return driver.findElement(By.cssSelector(cssLocator)).isDisplayed();
+    }
+
 }
--- src/test/java/stepDef/PlatejTests.java
+++ src/test/java/stepDef/PlatejTests.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import ru.yandex.qatools.allure.annotations.Description;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Description("Проверка модуля Платежи")
@@ -19,6 +20,7 @@ public class PlatejTests extends TestBase {
     WebDriverWait wait = new WebDriverWait(driver, 30);
     Actions act = new Actions(driver);
     JavascriptExecutor jse = (JavascriptExecutor)driver;
+
 
     public PlatejTests () throws Throwable {
         super();
@@ -76,17 +78,27 @@ public class PlatejTests extends TestBase {
         driver.findElement(By.xpath
                 ("//div[2]/div/div/div[2]/div/div/div/div/div/div/table/tbody/tr/td[2]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td[2]/em/button")).click(); //клик применить
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.cssSelector("div> div:nth-child(5) > table > tbody > tr > td.x-grid3-col.x-grid3-cell.x-grid3-td-0.x-grid3-cell-first.x-grid3-check-col-td")));
-
+        Thread.sleep(3000);
+         //wait.until(ExpectedConditions.visibilityOfElementLocated(
+        //By.cssSelector(".x-grid-group-body > div:nth-of-type(1) > table > tbody > tr > td > div > div")));
+                 //        By.cssSelector("div.x-grid-group-body > div:nth-child(2) > table > tbody > tr > td.x-grid3-col.x-grid3-cell.x-grid3-td-0.x-grid3-cell-first.x-grid3-check-col-td > div > div")));
+                //By.cssSelector("div> div:nth-child(5) > table > tbody > tr > td.x-grid3-col.x-grid3-cell.x-grid3-td-0.x-grid3-cell-first.x-grid3-check-col-td")));
+           // By.cssSelector("div > table > tbody > tr > td.x-grid3-col.x-grid3-cell.x-grid3-td-0.x-grid3-cell-first.x-grid3-check-col-td.x-grid3-dirty-cell > div > div")));
                 //By.cssSelector("div > div:nth-child(5) > table > tbody > tr > td.x-grid3-col.x-grid3-cell.x-grid3-td-0.x-grid3-cell-first.x-grid3-check-col-td > div > div")));
         //By.cssSelector("div > div:nth-child(2) > table > tbody > tr > td.x-grid3-col.x-grid3-cell.x-grid3-td-0.x-grid3-cell-first.x-grid3-check-col-td > div > div")));
 
-        //driver.findElement(By.cssSelector("div > div:nth-child(2) > table > tbody > tr > td.x-grid3-col.x-grid3-cell.x-grid3-td-0.x-grid3-cell-first.x-grid3-check-col-td > div > div")).click();
-        driver.findElement(By.cssSelector("div> div:nth-child(5) > table > tbody > tr > td.x-grid3-col.x-grid3-cell.x-grid3-td-0.x-grid3-cell-first.x-grid3-check-col-td")).click();
+        //driver.findElement(By.cssSelector("div.x-grid-group-body > div:nth-child(2) > table > tbody > tr > td.x-grid3-col.x-grid3-cell.x-grid3-td-0.x-grid3-cell-first.x-grid3-check-col-td > div > div")).click();
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div> div.x-grid3-row.x-grid3-row-first > table > tbody > tr > td.x-grid3-col.x-grid3-cell.x-grid3-td-1 > div")));
-        driver.findElement(By.cssSelector("div> div.x-grid3-row.x-grid3-row-first > table > tbody > tr > td.x-grid3-col.x-grid3-cell.x-grid3-td-1 > div")).isDisplayed(); //отображается список
+        List<WebElement> elements = driver.findElements(By.cssSelector(".x-grid-group-body > div:nth-of-type(1) > table > tbody > tr > td > div > div"));
+        if (elements.get(1).isDisplayed()) {
+            elements.get(1).click(); //выбрать второй элемент из списка
+        }
+        //if (app.isElementPresent(By.cssSelector(".x-grid-group-body > div:nth-of-type(1) > table > tbody > tr > td > div > div")))
+        //driver.findElement(By.cssSelector(".x-grid-group-body > div:nth-of-type(1) > table > tbody > tr > td > div > div")).click();
+        //driver.findElement(By.cssSelector("div.x-grid-group-body > div:nth-child(3) > table > tbody > tr > td.x-grid3-col.x-grid3-cell.x-grid3-td-0 > div > div")).click();
+        Thread.sleep(3000);
+
+                
     }
 
     @Then ("^Выбрать Документы$")
